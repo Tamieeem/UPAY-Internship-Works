952ebf5216fa7fac9ed33d62407e8ba6989a009d
Tamieeem <taskinhassanador177@gmail.com>
2026-06-16 13:12:06 +0600

    feat: added nested serializers

---
+++ apps/accounts/nested_serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import Account, Transaction
+from django.db import transaction as trx
+from utils.choices import TransactionType
+
+
+class UserMiniSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()          
+        fields = ["id", "username", "email"]
+
+class AccountNestedSerializer(serializers.ModelSerializer):
+    user = UserMiniSerializer(read_only=True)    
+
+    class Meta:
+        model = Account
+        fields = ["id", "account_number", "status", "balance", "user"]
+
+class TransactionNestedSerializer(serializers.ModelSerializer):
+    from_account = AccountNestedSerializer(read_only=True)
+    to_account   = AccountNestedSerializer(read_only=True)
+
+    class Meta:
+        model = Transaction
+        fields = [
+                "id", 
+                "reference_id", 
+                "from_account", 
+                "to_account", 
+                "amount", 
+                "transaction_type", 
+                "status"
+        ]
+
+class TransactionWriteSerializer(serializers.ModelSerializer):
+    from_account = AccountNestedSerializer()
+    to_account = AccountNestedSerializer()
+    class Meta:
+        model = Transaction
+        fields = ["id", "reference_id", "from_account", "to_account", "amount", "transaction_type", "status"]
+        read_only_fields = ["id", "reference_id", "status"]
+
+    def create(self, validated_data):
+        #accounts pop is must as account table doesnt have such field
+        account_data = validated_data.pop("from_account")
+        to_data = validated_data.pop("to_account")
+        with trx.atomic():
+            from_account = Account.objects.create(**account_data)    
+            to_account = Account.objects.create(**to_data)
+            transaction = Transaction.objects.create(
+                from_account = from_account,
+                to_account = to_account,
+                **validated_data
+            )
+            
+            return transaction
