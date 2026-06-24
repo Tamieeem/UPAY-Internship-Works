5439a592944b30d7ab1f5ed1e5bfb2e8af9b7d20
Tamieeem <taskinhassanador177@gmail.com>
2026-06-24 15:32:26 +0600

    feat: added urls for jwt token, register, logout views

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import(
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+from rest_framework.authtoken.views import obtain_auth_token
+
 from .views import (
     AccountAPIView,
     accountGenericView,
@@ -7,7 +14,8 @@ from .views import (
     TransactionModelViewSet,
     TransactionRefundAPIView,
     AccountFreezeAPIView,
-    NestedTransactionCreateAPIView
+    NestedTransactionCreateAPIView,
+    LogoutView
 )
 
 router = DefaultRouter()
@@ -15,15 +23,28 @@ router.register('accounts', AccountModelViewSet, basename="account")
 router.register('transactions', TransactionModelViewSet, basename="transaction")
 
 urlpatterns = [
+    #auth urls-
+    #token-auth
+    path('api/v1/auth/token/', obtain_auth_token, name='drf-token'), #default drf token
+    #jwt token
+    
+    path('api/v1/auth/jwt/create/', TokenObtainPairView.as_view(), name='jwt-create'),
+    path('api/v1/auth/jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
+    path('api/v1/auth/jwt/verify/', TokenVerifyView.as_view(), name='jwt-verify'),
+    
+    path("api/v1/auth/logout/", LogoutView.as_view(), name="logout"),
+
+    
+    #views-api-endpoints
     path('api/v1/raw/accounts/', AccountAPIView.as_view()),
     #for patch/delete apiview - same url
     path('api/v1/raw/accounts/<uuid:id>/freeze/', AccountFreezeAPIView.as_view()),
     path('api/v1/raw/accounts/<uuid:id>/', AccountAPIView.as_view()),
     path('api/v1/raw/transactions/<uuid:id>/refund/', TransactionRefundAPIView.as_view()),
     
+    
     #nested write apiview endpoint
     path('api/v1/raw/transactions/create-nested/', NestedTransactionCreateAPIView.as_view()),
-    
     
     
     #generic apiviews urls
