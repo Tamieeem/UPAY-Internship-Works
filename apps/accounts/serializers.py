86eb81f78500c2fa1e7457777a8941dc55b2c0f2
Tamieeem <taskinhassanador177@gmail.com>
2026-06-24 15:30:10 +0600

    fead: added RegisterUserSerializer for User Registration

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,5 +1,9 @@
 from .models import Account, Transaction
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
 
 class AccountSerializer(serializers.ModelSerializer):
     
@@ -40,3 +44,27 @@ class TransactionSerializer(serializers.ModelSerializer):
         # 'status',
         'reversal_of'
         ]
+        
+class RegisterUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only = True,
+        required = True,
+        validators = [validate_password],
+        style = {'input_type': 'password'}
+    )
+    
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        read_only_fields = ["id"]
+        
+    def create(self, validated_data):
+        #pop password for security and leakage(such as logging.)
+        password = validated_data.pop('password')
+        
+        #create_user is must for avoiding plaintext password saving.
+        #it hashes the password and saves.
+        user = User.objects.create_user(
+            password=password, **validated_data 
+        )
+        return user
